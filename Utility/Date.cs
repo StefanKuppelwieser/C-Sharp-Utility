c0eeef25ea11eee0a033a3c1327b3070e11babee
Stefan Kuppelwieser <OpenSource@Kuppelwieser.net>
2018-11-06 19:13:47 +0100

    Added a Method that converrts a date of type string to date of type datetime

--- Utility/Date.cs
+++ Utility/Date.cs
@@ -37,9 +37,16 @@ namespace Utility
         /// <returns>Return the date in datetime format</returns>
         public static DateTime ConvertDateToDatetime(string dd, string mm, string yyyy)
         {
-            DateTime dt;
+            DateTime dt = default(DateTime);
 
-            DateTime.TryParseExact(dd + "/" + mm + "/" + yyyy, "d/M/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dt);
+            try
+            {
+                DateTime.TryParseExact(dd + "/" + mm + "/" + yyyy, "d/M/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
 
             return dt;
         }
@@ -53,11 +60,39 @@ namespace Utility
         /// <returns>Return the date in datetime format</returns>
         public static DateTime ConvertDateToDatetime(int dd, int mm, int yyyy)
         {
-            DateTime dt;
+            DateTime dt = default(DateTime);
 
-            DateTime.TryParseExact(dd + "/" + mm + "/" + yyyy, "d/M/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dt);
+            try
+            {
+                DateTime.TryParseExact(dd + "/" + mm + "/" + yyyy, "d/M/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
 
             return dt;
+        }
+
+        /// <summary>
+        /// It converts a date in type of string in type Dateime
+        /// </summary>
+        /// <param name="datetime">Contains the date as string</param>
+        /// <returns>Return the date in type Datetime in Format 'yyyy-MM-dd HH:mm:ss.fff'</returns>
+        public static DateTime ConvertStringToDatetime(string datetime)
+        {
+            DateTime myDate = default(DateTime);
+
+            try
+            {
+                myDate = DateTime.ParseExact(datetime, "yyyy-MM-dd HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return myDate;
         }
     }
 }
