47f3c605fdcb459f685707b694f77fff24938288
Stefan Kuppelwieser <OpenSource@Kuppelwieser.net>
2018-11-06 14:56:13 +0100

    Unit test for class Type has been created

---
+++ UtilityTests/TypeTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Tests
+{
+    [TestClass()]
+    public class TypeTests
+    {
+        [TestMethod()]
+        public void ConvertStringToIntTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConvertStringToDoubleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsStringEmptyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConvertWindowsPointToDrawingPointTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
